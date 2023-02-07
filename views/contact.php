dc257880dc82d639d33f7e606fbad782f830aaa6
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-02-07 15:55:10 +0200

    Basic contact form

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,17 @@
 <h1>Contact</h1>
+
+<form>
+    <div class="form-group mt-2">
+        <label for="exampleFormControlInput1">Subject</label>
+        <input type="subject" class="form-control" id="exampleFormControlInput1" placeholder="Subject">
+    </div>
+    <div class="form-group mt-2">
+        <label for="exampleFormControlInput1">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="form-group mt-2">
+        <label for="exampleFormControlTextarea1">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+</form>
