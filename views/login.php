87d7d174be4a18808abfd53117caf11b016f6f5d
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-02-15 19:47:04 +0200

    Init new views.

---
+++ views/login.php
@@ -0,0 +1,17 @@
+<h1>Log in</h1>
+
+<form action="" method="POST">
+    <div class="form-group mt-2">
+        <label>Subject</label>
+        <input type="text" name="subject" class="form-control" placeholder="Subject">
+    </div>
+    <div class="form-group mt-2">
+        <label>Email address</label>
+        <input type="email" name="email" class="form-control" placeholder="name@example.com">
+    </div>
+    <div class="form-group mt-2">
+        <label>Body</label>
+        <textarea class="form-control" name="body" rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+</form>
---
+++ views/register.php
@@ -0,0 +1,34 @@
+<h1>Create an Account</h1>
+
+<form action="" method="POST">
+    <div class="row">
+        <div class="col">
+            <div class="form-group mt-2">
+                <label>Firstname</label>
+                <input type="text" name="firstname" class="form-control" placeholder="Firstname">
+            </div>
+
+        </div>
+        <div class="col">
+            <div class="form-group mt-2">
+                <label>Lastname</label>
+                <input type="text" name="lastname" class="form-control" placeholder="Lastname">
+            </div>
+        </div>
+    </div>
+
+
+    <div class="form-group mt-2">
+        <label>Email address</label>
+        <input type="email" name="email" class="form-control" placeholder="name@example.com">
+    </div>
+    <div class="form-group mt-2">
+        <label>Passsword</label>
+        <input type="password" name="password" class="form-control" placeholder="Enter your password">
+    </div>
+    <div class="form-group mt-2">
+        <label>Confirm Password</label>
+        <input type="password" name="confirmPassword" class="form-control" placeholder="Enter your password">
+    </div>
+    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+</form>
