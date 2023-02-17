db8d6139d2cf40550ab459b6e2e8cbe1c4264800
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-02-17 19:40:50 +0200

    Layouts-view

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -22,9 +22,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/contact">Contact</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/about">About</a>
-                    </li>
                 </ul>
 
                     <ul class="navbar-nav ms-auto">
