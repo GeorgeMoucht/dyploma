193242387af2ca2470b6409ef7a0d7a07e96628e
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-02-28 12:22:28 +0200

    Implement rule_required validation

--- core/Model.php
+++ core/Model.php
@@ -14,7 +14,20 @@ namespace app\core;
 
 abstract class Model
 {
+    public const RULE_REQUIRED = 'required';
+    public const RULE_EMAIL = 'email';
+    public const RULE_MIN = 'min';
+    public const RULE_MAX = 'max';
+    public const RULE_MATCH = 'match';
+
+    public array $errors = [];  // Store all errors from validate(), and send them back to the controller.
+
+
+    // Since this function is abstract, every child class should implement this function.
+    abstract public function rules(): array;
+
     // Read data that are posted by the user.
+    // Before every validation of a model, loadData() is required to be called.
     public function loadData($data)
     {
         foreach($data as $key => $value) {
@@ -29,9 +42,44 @@ abstract class Model
         }
     }
 
+    // Validate all the rules variables that model has defined.
     public function validate()
-    {
+    {   
+        // Each attribute can have multiple rules.
+        // So we need to iterrate over all the rules of each attribute.
+        foreach($this->rules() as $attribute => $rules) {
+            $value = $this->{$attribute};
+            foreach($rules as $rule) {
+                $ruleName = $rule;
+                if(!is_string($ruleName)) {
+                    $ruleName = $rule[0];
+                }
+                
+                // Validate RULE_REQUIRED
+                if($ruleName === self::RULE_REQUIRED && !$value) {
+                    $this->addError($attribute, self::RULE_REQUIRED);
+                }
+            }
+        }
 
+        return empty($this->errors);
+    }
+
+    public function addError(string $attribute , string $rule)
+    {
+        $message = $this->errorMessages()[$rule] ?? '';
+        $this->errors[$attribute][] = $message;
+    }
+
+    public function errorMessages()
+    {
+        return [
+            self::RULE_REQUIRED => 'This field is required',
+            self::RULE_EMAIL => 'This field must be a valid email address',
+            self::RULE_MIN => 'Minimum length of this field must be {min}',
+            self::RULE_MAX => 'Maximum length of this field must be {max}',
+            self::RULE_MATCH => 'This field must be the same as {match}',
+        ];
     }
 }
 
--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -27,6 +27,17 @@ class RegisterModel extends Model
         echo "Creating this user.";
     }
 
+    // Validation rules.
+    public function rules(): array
+    {
+        return [
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED , self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED , [self::RULE_MIN , 'min' => 8] , [self::RULE_MAX , 'max'=> 24]],
+            'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH , 'match' => 'password']],
+        ];
+    }
 
 }
 
