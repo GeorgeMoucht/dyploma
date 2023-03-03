651a610f8cd55adfa59c3df14c9742d6af9ba55b
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-03-03 20:21:00 +0200

    Create basic database connect

---
+++ migrations/m1_initial.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class m1_initial
+ * 
+ * @author George Mouchtaridis <giorgosmoucht@gmail.com>
+ * @package
+*/
+
+
+class m1_initial
+{
+    public function up()
+    {
+        echo "Applying migrations";
+    }
+
+
+    public function down()
+    {
+        echo "Down migrations";
+    }
+}
+
+?>
