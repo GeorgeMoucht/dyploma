8362608e27af45226b48442bdb7d54b5ef3faff8
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-04-14 20:00:03 +0300

    create basic navigation-menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <link rel="stylesheet" href="stylesheets/main.css">
 </head>
 <body>
@@ -17,20 +18,23 @@
             <span>LOGO</span>
         </div>
         <div class="menu col">
-            <ul class="menu-list">
+            <ul class="menu-list fl-row">
                 <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
+                <li>Courses</li>
+                <li>Forum</li>
             </ul>
         </div>
-        
-        <div class="burger-icon col">
-            <button class="hamburger">
-                <div class="bar"></div>
-            </button>
+
+        <div class="image-cnt my-courses-btn col fl-row">
+            <img class="profile-image" src="images/default-avatar.png" alt="Profile image">    
+            <span>My Courses</span>
+        </div>
+
+        <div class="notifications col fl-col">
+            <i class="bi bi-bell"></i>
         </div>
     </div>
+
 
 
     <?php if (Application::$app->session->getFlash('success')):  ?>
